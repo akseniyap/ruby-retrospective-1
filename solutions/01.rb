8e3eee5340b728ba3dac49f6ceea91f97eef28a7
Akseniya Penkova <akseniya.penkova@gmail.com>
2011-11-13 18:42:39 +0200

    No need to use 'self' although it's strange without it. But then why I can't slice the array without explicitly using self?

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,27 +1,27 @@
 class Array
   def to_hash
     hash = {}
-    self.each { |element| hash[element[0]] = element[1] }
+    each { |element| hash[element[0]] = element[1] }
     hash
   end
 
   def index_by &block
     hash = {}
-    self.each { |element| hash[block.call(element)] = element }
+    each { |element| hash[block.call(element)] = element }
     hash
   end
 
   def subarray_count subarray
-    return -1 if subarray == [] || self.size < subarray.size
+    return -1 if subarray == [] || size < subarray.size
 
     count = 0
-    0.upto(self.size - 1) { |i| count += 1 if subarray == self[i, subarray.size] }
+    0.upto(size - 1) { |i| count += 1 if subarray == self[i, subarray.size] }
     count
   end
 
   def occurences_count
     hash = Hash.new(0)
-    self.each { |element| hash[element] += 1 }
+    each { |element| hash[element] += 1 }
     hash
   end
 end
